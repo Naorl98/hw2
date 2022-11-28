f7616e7aba8fb573fd880d434ad3cf6edfb61988
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-11-28 18:58:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 #include "my_mat.h"
-
-
-int main(){
-
-    char func;
-    int routeArray[10][10];
-    int matArray[10][10];
-    while(func != 'D'){
-        scanf(" %c", &func);
-        if(func == 'A'){
-            matArray = A();
-            routeArray= makeRoute(matArray);        }
-        else if (func =='B'){
-            B(routeArray);
-            printf("\n");
-        }
-        else if(func == 'C'){
-            C(routeArray);
-            printf("\n");
+int A(){
+    int arr[10][10];
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            int number;
+            scanf(" %d", &number);
+            arr[i][j]= number;
         }
     }
-    return 0;
+    int *p = &arr;
+    return *p;
+}
+
+void B(int arr[10][10]){
+    int i, j;
+    scanf(" %d %d", &i, &j);
+    if(arr[i][j] !=0 || arr[j][i] != 0) printf("True");
+    else printf("False");
+  
+}
+int makeRoute(int arr[10][10]){
+        for (int j = 0; j < 10; j++)
+        {
+             arr[j][j]= 0 ;
+        }    
+    for(int k=0;k<10;k++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			for(int j=0;j<10;j++)
+			{
+				if(arr[i][j]>arr[i][k]+arr[k][j])
+				{
+					arr[i][j]=arr[i][k]+arr[k][j];
+				}
+		    }
+	    }
+	}
+    int *p = &arr;
+    return *p;
+}
+void C(int arr[10][10]){
+    int i, j;
+    scanf(" %d %d", &i, &j);
+    if(arr[i][j] == 0)
+        printf("-1");
+    else
+    printf(" %ls", &arr[i][j]);
 }
