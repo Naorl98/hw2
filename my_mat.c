308543043ffb50ac44119f083a4b67df6125262e
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-11-29 18:59:33 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include "my_mat.h"
-void A(int arr[10][10]){
+
+void A(int crate[10][10]){
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
             int number;
-            scanf(" %d", &number);
-            arr[i][j]= number;
+            scanf("%d\n",&number);
+            crate[i][j]= number;
         }
     }
 }
 
-void B(int arr[10][10]){
+void B(int TorF[10][10]){
     int i, j;
-    scanf(" %d %d", &i, &j);
-    if(arr[i][j] !=0 || arr[j][i] != 0) printf("True");
-    else printf("False");
+    scanf("%d %d\n", &i, &j);
+    if(TorF[i][j] == 99999999 || TorF[i][j] <= 0)
+        printf("False\n");
+    else 
+        printf("True\n");
   
 }
-void makeRoute(int arr[10][10]){
+void makeRoute(int route[10][10]){
+	for(int i=0;i<10;i++){
+		for(int j=0;j<10;j++){
+            if(route[i][j] == 0)
+                route[i][j] = 99999999;
+        }
+    }     
     for (int i = 0; i < 10; i++)
     {
-        arr[i][i]=0;
+        route[i][i]=0;
     }
     
     for(int k=0;k<10;k++)
@@ -28,20 +37,27 @@ void makeRoute(int arr[10][10]){
 		for(int i=0;i<10;i++)
 		{
 			for(int j=0;j<10;j++)
-			{	if(arr[i][j] != 0){
-				if(arr[i][j]>arr[i][k]+arr[k][j])
-				{
-					arr[i][j]=arr[i][k]+arr[k][j];
-				}}
+			{	
+                    
+				if(route[i][j]>route[i][k]+route[k][j])
+				{ 
+                    if(route[i][k]+route[k][j]<0){
+                        if(route[i][k]==99999999) 
+                            route[i][j]= route[k][j];
+                        else
+                            route[i][j]=  route[i][k];
+                    } 
+					route[i][j]=route[i][k]+route[k][j];
+				}
 		    }
 	    }
 	}
 }
-void C(int arr[10][10]){
+void C(int shorter[10][10]){
     int i, j;
-    scanf(" %d %d", &i, &j);
-    if(arr[i][j] == 0)
-        printf("-1");
-    else
-    printf(" %d", arr[i][j]);
+    scanf("%d %d\n", &i, &j);
+    if (shorter[i][j] == 99999999 || shorter[i][j] <= 0 ) 
+        printf("-1\n");
+    else if(i==j) printf("-1\n");
+    else printf("%d\n",shorter[i][j]);
 }
